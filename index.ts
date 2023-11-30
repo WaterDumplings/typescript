c031b8da4da3e6e8fb6f471756c794ae65cdbb0e
김성준 <q881229@gmail.com>
2023-11-30 17:50:22 +0900

    typescript_1

---
+++ index.ts
@@ -0,0 +1,19 @@
+type Name = string | number
+
+let 이름 :Name = 132;
+
+function 함수(x :number) :number {
+  return x * 2 
+}
+
+type Member = {
+  [key : string] : string
+}
+let john :Member = { name : 'kim' }
+
+class User {
+  name :string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
